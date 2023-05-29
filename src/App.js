61cf64a5ecdd791adb3cdb4d2ff6dd244ebaea11
namjunpark <67637245+joon623@users.noreply.github.com>
2023-05-29 16:25:34 +0900

    url 주소 변경 sign-in > sing-up

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           .then((idToken) => {
             // TODO: Send the ID token to the server
             console.log("idToken", idToken);
-            fetch("https://winners-high-be-test.herokuapp.com/auth/sign-in", {
+            fetch("https://winners-high-be-test.herokuapp.com/auth/sign-up", {
               method: "post",
               headers: {
                 Authorization: `Bearer ${idToken}`,
